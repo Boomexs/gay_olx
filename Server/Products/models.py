899db539d3fb577c86d5d8a313d2d0cc46099f3b
= <=>
2025-01-29 19:33:41 +0100

    odrobine dodane do bazy danych

--- Server/Products/models.py
+++ Server/Products/models.py
@@ -6,7 +6,7 @@ class Product(models.Model):
     name = models.CharField(max_length=100)
     price = models.DecimalField(
         max_digits=10,
-        decimal_places=1,  # 1 decimal place
+        decimal_places=2,  # 1 decimal place
         validators=[
             MinValueValidator(0),  # Price must be >= 0
         ]
